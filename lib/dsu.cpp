346b90f84f7cbab9d3cf5f1bdca7d8cfc0837d46
Semeen <semenprofis999@gmail.com>
2024-12-08 00:32:06 +0300

    lib; dsu

--- lib/dsu.cpp
+++ lib/dsu.cpp
@@ -1,29 +1,63 @@
 // semenInRussia 2024
 // Disjoin System Union (DSU), very usable thing
 #include <iostream>
+#include <vector>
 using namespace std;
 
-const int N = 1e6;
-int p[N];
-int64_t a[N];
+struct dsu {
+  vector<int> p, sz;
 
-int get(int i) {
-  if (p[i] == i) {
-    return i;
+  dsu(int n) { clear(n); }
+
+  // k - new size, maybe (-1 - no change)
+  void clear(int k = -1) {
+    if (k == -1) {
+      k = int(p.size());
+    }
+    p.resize(k);
+    sz.assign(k, 1);
+    for (int i = 0; i < k; i++) {
+      p[i] = i;
+    }
   }
-  return p[i] = get(p[i]);
-}
 
-int join(int a, int b) {
-  b = get(b);
-  a = get(a);
-  return p[b] = a;
-}
+  inline void resize(int k) { clear(k); }
+
+  int get(int i) {
+    if (p[i] == i) {
+      return i;
+    }
+    return p[i] = get(p[i]);
+  }
+
+  int join(int a, int b) {
+    b = get(b);
+    a = get(a);
+    if (sz[b] > sz[a]) {
+      swap(a, b);
+    }
+    sz[a] += sz[b];
+    return p[b] = a;
+  }
+};
 
 int main() {
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    p[i] = i;
+  cin.tie(nullptr)->sync_with_stdio(0);
+  //--
+
+  int n, m;
+  cin >> n >> m;
+  dsu ds(n);
+  for (int i = 0; i < m; i++) {
+    string t;
+    int a, b;
+    cin >> t >> a >> b;
+    a--, b--;
+    if (t == "union") {
+      ds.join(a, b);
+    } else {
+      bool ok = ds.get(a) == ds.get(b);
+      cout << (ok ? "YES" : "NO") << "\n";
+    }
   }
 }
